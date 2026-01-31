99be78129869817cdb5863a0613bf866e420c238
Tazi Aymane <taziaymane68@gmail.com>
2026-01-31 19:18:48 +0100

    Navigation Basics with Expo Router - Creating and Linking New Screens - Styling and Customizing Navigation Links

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import LoginScreen from "./login";
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return <>
+
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home"}} />
+      <Stack.Screen name="login" options={{ title:"Login"}} />
+    </Stack>;
+  </>
+
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
-
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.View}>
+
+      <Text>Hello </Text>
+      <Link href={"/login"}
+      style={style.navButton}
+      >Login Page</Link>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  View : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navButton : {
+    width: 100,
+    height: 20,
+    backgroundColor:"coral",
+    borderRadius:8,
+    textAlign:"center"
+  }
+})
